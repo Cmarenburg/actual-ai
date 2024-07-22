cefe7283c4de45c27b949b9b5931550032419082
Colin Arenburg <colin@arenburg.land>
2024-07-21 21:35:54 -0300

    Reverts to default matching

--- src/transaction-service.js
+++ src/transaction-service.js
@@ -6,8 +6,6 @@ const { suppressConsoleLogsAsync } = require('./utils');
 const NOTES_NOT_GUESSED = 'actual-ai could not guess this category';
 const NOTES_GUESSED = 'actual-ai guessed this category';
 
-// const ADDITIONAL_EXCLUDES = process.env.ADDITIONAL_EXCLUDES.split(',');
-
 function findUUIDInString(str) {
   const regex = /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}/g;
   const matchResult = str.match(regex);
@@ -33,51 +31,33 @@ async function processTransactions() {
   const categories = await actualApi.getCategories();
   const payees = await actualApi.getPayees();
   const transactions = await actualApi.getTransactions();
-
   const uncategorizedTransactions = transactions.filter(
     (transaction) => !transaction.category
-      && transaction.transfer_id === null
-      && transaction.starting_balance_flag !== true
-      // This line handles the null `notes`
-      && (transaction.notes ? !transaction.notes.includes(NOTES_NOT_GUESSED) : true),
-    // && ADDITIONAL_EXCLUDES.some((exclude) => transaction.imported_payee.includes(exclude)),
+          && transaction.transfer_id === null
+          && transaction.starting_balance_flag !== true
+          && transaction.notes.includes(NOTES_NOT_GUESSED) === false,
   );
 
-  console.log(uncategorizedTransactions);
-
-  async function processTransaction(transaction, index, total) {
-    console.log(`${index + 1}/${total} Processing transaction ${transaction.imported_payee} / ${transaction.notes} / ${transaction.amount}`);
-
-    // If it's a parent transaction, process each child transaction
-    if (transaction.is_parent) {
-      // for (const child of transaction.subtransactions) {
-      //   await processTransaction(child, index, total); // Recursive call for each child
-      // }
-      return; // Skip the rest of the processing for the parent transaction
-    }
-
-    // Processing logic for a "normal" transaction or child transaction
+  for (let i = 0; i < uncategorizedTransactions.length; i++) {
+    const transaction = uncategorizedTransactions[i];
+    console.log(`${i + 1}/${uncategorizedTransactions.length} Processing transaction ${transaction.imported_payee} / ${transaction.notes} / ${transaction.amount}`);
     const guess = await ask(categoryGroups, transaction, payees);
     const guessUUID = findUUIDInString(guess);
     const guessCategory = categories.find((category) => category.id === guessUUID);
 
     if (!guessCategory) {
-      console.warn(`${index + 1}/${total} OpenAI could not classify the transaction. OpenAI's guess: ${guess}`);
+      console.warn(`${i + 1}/${uncategorizedTransactions.length} OpenAI could not classify the transaction. OpenAIs guess: ${guess}`);
       await actualApi.updateTransaction(transaction.id, {
         notes: `${transaction.notes} | ${NOTES_NOT_GUESSED}`,
       });
-      return;
+      continue;
     }
-    console.log(`${index + 1}/${total} Guess: ${guessCategory.name}`);
+    console.log(`${i + 1}/${uncategorizedTransactions.length} Guess: ${guessCategory.name}`);
 
     await actualApi.updateTransaction(transaction.id, {
       category: guessCategory.id,
       notes: `${transaction.notes} | ${NOTES_GUESSED}`,
     });
-  }
-
-  for (let i = 0; i < uncategorizedTransactions.length; i++) {
-    await processTransaction(uncategorizedTransactions[i], i, uncategorizedTransactions.length);
   }
 }
 module.exports = {
